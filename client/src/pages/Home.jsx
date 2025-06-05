8ed90c030fb87050cbf8ee9b278b4df7578a209b
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-05 11:19:01 +0530

    second

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
+import Header from "../components/Header.jsx";
 
 const Home = () => {
 	return (
 		<>
 		<Navbar />
+<Header />
 		</>
 	);
 };
