1579369d51c06f53a4b7a8eb8bb6fcb073c8b703
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-06 22:49:40 +0530

    third

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from "../components/Navbar.jsx";
 import Header from "../components/Header.jsx";
+import BlogList from "../components/BlogList.jsx";
 
 const Home = () => {
 	return (
 		<>
 		<Navbar />
 <Header />
+	<BlogList />
 		</>
 	);
 };
