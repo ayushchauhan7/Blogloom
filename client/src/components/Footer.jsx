52bd6ae2bca8f8b404892f7e8c41ad77f73bf742
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-10 21:55:48 +0530

    fifth

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {assets} from "../assets/assets.js";
+import {assets, footer_data} from "../assets/assets.js";
 
 const Footer = () => {
 	return (
@@ -10,8 +10,19 @@ const Footer = () => {
 	<p className='max-w-[410px] mt-6'></p>
 </div>
 
-				<div className='flex flex-wrap justify-between w-full md:w-[45%] gap-5'>
-
+				<div className='flex flex-w	rap justify-between w-full md:w-[45%] gap-5'>
+					{footer_data.map((section, index) => (
+						<div key={index}>
+<h3 className='font-semibold text-base text-gray-900 md:mb-5 mb-2'>{section.title}</h3>
+							<ul className='text-sm space-y-1'>
+								{section.links.map((link, i) => (
+									<li key={i}>
+										<a href="#" className='hover:underline transition'>{link}</a>
+									</li>
+								))}
+							</ul>
+						</div>
+					))}
 				</div>
 
 			</div>
